81e7fbc29564be9367d0710fe74c265d41e56990
Piyush Rajput <rajputpiyush0773@gmail.com>
2023-08-05 11:09:54 +0530

    initialize react chat app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { Home, SignIn, SignUp,Profile, NotFound } from "./pages";
+import { Home, SignIn, SignUp, Profile, NotFound } from "./pages";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./assets/Styles/style.scss";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
-
 
 function App() {
   // current user details through useContext
@@ -17,32 +16,29 @@ function App() {
     }
     return children;
   };
+
+
   return (
     <div className="App">
       <BrowserRouter basename="/my-chat-app">
         <ToastContainer/>
         <Routes>
-          <Route exact path="/">
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="login" element={<SignIn />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route
-              path="profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<NotFound/>}/>
-          </Route>
+          {/* Define the main route with a component or element */}
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          {/* Define your other nested routes here */}
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="login" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route
+            path="profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
     </div>
@@ -50,3 +46,4 @@ function App() {
 }
 
 export default App;
+
